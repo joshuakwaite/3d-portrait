4a3f322467880994ee686aad51780aa612ace6e7
Josh Waite <kalewaite@gmail.com>
2017-04-17 10:40:28 -0600

    Adding & updating pages

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -2,42 +2,5 @@ var app = angular.module("3dportraitApp");
 
 app.controller("contactController", ["$scope", "userService", "objectService", function($scope, userService, objectService) {
 
-    $scope.test = "contact";
-
-    $scope.submitForm = function (name) {
-
-        userService.getUsers("items").then(function(res){
-            console.log(res.data)
-        })
-    };
-
-    $scope.signIn = function(username, password) {
-        userService.signIn(username, password).then(function(res){
-            console.log("signin succeeded with user :" + res.data.username);
-            console.log(res.data);
-        })
-    };
-
-    $scope.signOut = function() {
-        userService.signOut().then(function(res){
-            console.log(res.data)
-        })
-    };
-
-    $scope.newObject = function(object) {
-        objectService.createObject(object)
-    };
-
-    $scope.getInfo = function() {
-        userService.getUserInfo().then(function(res){
-            console.log(res.data)
-        })
-    };
-
-    $scope.signUp = function (firstName, lastName, email, password, confirmPassword) {
-        userService.signUp(firstName, lastName, email, password, confirmPassword).then(function(res){
-            console.log(res.data)
-        })
-    };
 
 }]);
--- app/product/product.js
+++ app/product/product.js
@@ -2,6 +2,11 @@ var app = angular.module("3dportraitApp");
 
 app.controller("productController", ["$scope", function($scope) {
 
-    $scope.test = "product"
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'awesome',
+        clientId: '933754b3d5c44b46925ed5294cfa17a1'
+    });
+    feed.run();
 
 }]);
