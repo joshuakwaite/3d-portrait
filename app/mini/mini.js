8b11b5be4c14c104cad31cf0f25f61d01ffc24cb
Josh Waite <kalewaite@gmail.com>
2017-04-27 09:19:04 -0600

    Edited text

--- app/mini/mini.js
+++ app/mini/mini.js
@@ -190,109 +190,109 @@ app.controller("miniController", ["$scope", function ($scope) {
             Size: 2,
             Price: 54.95,
             Additional: 34.95,
-            Under: 0
+            Under: "Unavailable"
         },
         {
             Size: 3,
             Price: 64.95,
             Additional: 44.95,
-            Under: 0
+            Under: "Unavailable"
         },
         {
             Size: 4,
             Price: 74.95,
             Additional: 54.95,
-            Under: 37.95
+            Under: "$37.95"
         },
         {
             Size: 5,
             Price: 84.95,
             Additional: 64.95,
-            Under: 42.95
+            Under: "$42.95"
         },
         {
             Size: 6,
             Price: 99.95,
             Additional: 79.95,
-            Under: 49.95
+            Under: "$49.95"
         },
         {
             Size: 7,
             Price: 114.95,
             Additional: 89.95,
-            Under: 57.95
+            Under: "$57.95"
         },
         {
             Size: 8,
             Price: 129.95,
             Additional: 104.95,
-            Under: 64.95
+            Under: "$64.95"
         },
         {
             Size: 9,
             Price: 149.95,
             Additional: 124.95,
-            Under: 74.95
+            Under: "$74.95"
         },
         {
             Size: 10,
             Price: 174.95,
             Additional: 149.95,
-            Under: 87.95
+            Under: "$87.95"
         },
         {
             Size: 11,
             Price: 204.95,
             Additional: 174.95,
-            Under: 102.95
+            Under: "$102.95"
         },
         {
             Size: 12,
             Price: 244.95,
             Additional: 214.95,
-            Under: 122.95
+            Under: "$122.95"
         },
         {
             Size: 13,
             Price: 294.95,
             Additional: 264.95,
-            Under: 147.95
+            Under: "$147.95"
         },
         {
             Size: 14,
             Price: 349.95,
             Additional: 309.95,
-            Under: 174.95
+            Under: "$174.95"
         },
         {
             Size: 15,
             Price: 409.95,
             Additional: 359.95,
-            Under: 204.95
+            Under: "$204.95"
         },
         {
             Size: 16,
             Price: 474.95,
             Additional: 419.95,
-            Under: 239.95
+            Under: "$239.95"
         },
         {
             Size: 17,
             Price: 544.95,
             Additional: 484.95,
-            Under: 272.95
+            Under: "$272.95"
         },
         {
             Size: 18,
             Price: 619.95,
             Additional: 554.95,
-            Under: 309.95
+            Under: "$309.95"
         },
         {
             Size: 19,
             Price: 699.95,
             Additional: 629.95,
-            Under: 339.95
+            Under: "$339.95"
         }
     ];
 
